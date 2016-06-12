6a4b64e54033d05717ff7724dca57aafe1b1100b
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 14:53:10 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/system.nrepl "0.1.0-SNAPSHOT"
+(defproject sv/system.nrepl "0.1.0"
   :description "Components and helper functions to use a nREPL server
                 in a sv/system-based application."
   :url "https://github.com/SimpleValue/sv.system.nrepl"
