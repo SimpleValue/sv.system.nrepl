a748561c40b0d26e13fae9ed2b209b2660ceae4d
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 14:52:48 +0200

    Extracted sv.system.nrepl from sv/system.

---
+++ README.md
@@ -0,0 +1,12 @@
+# system.nrepl
+
+Components and helper functions to use a
+[nREPL](https://github.com/clojure/tools.nrepl) server in a
+sv/system-based application.
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sv/system.nrepl "0.1.0-SNAPSHOT"
+  :description "Components and helper functions to use a nREPL server
+                in a sv/system-based application."
+  :url "https://github.com/SimpleValue/sv.system.nrepl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.11"]])
